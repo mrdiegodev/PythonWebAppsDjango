7b2d718685edf60d929cca0b7b28a61cf625d242
Diego H. Laptop <hola@mrdiego.dev>
2023-07-18 12:58:17 -0600

    Started first DJANGO project

---
+++ IntroductionToDjango/introduction.py
@@ -0,0 +1,13 @@
+"""
+HTTP ---> Hyper Text Transfer Protocol
+TCP  ---> Transmission Control Protocol (manages the chanel between your browser and the server) used to manage connections
+WEB FRAMEWORK ---> DJANGO
+    CONFIGURING SETTINGS
+
+        settings.py and urls.py (DO NOT DELETE)
+
+        settings --> contains configurations that we'll be editing throughout the development 
+                        INSTALLED_APPS WHICH CONTAINS THE APPS THAT MAKE THE DJNAGO projecy.
+"""
+
+print('Hello')
